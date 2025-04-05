7dc58d2857a22433a2b422f68cabee29c01b504b
Subhamay-Dey <ncdey1966@gmail.com>
2025-04-05 13:16:17 +0530

    refactor: update ESLint configuration to allow unused variables prefixed with "_" and simplify file patterns

--- cuberto-clone-main/eslint.config.mjs
+++ cuberto-clone-main/eslint.config.mjs
@@ -12,8 +12,18 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    files: ["**/*.{js,ts,jsx,tsx}"],
     rules: {
+      // Allow unused vars/args if prefixed with "_"
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_"
+        }
+      ],
+      // Optional: allow use of `any`
       "@typescript-eslint/no-explicit-any": "off"
     }
   }
